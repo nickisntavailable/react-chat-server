8677fa735022355824706325c201771222b01121
nickisntavailable <62218429+nickisntavailable@users.noreply.github.com>
2020-08-04 15:38:22 +0300

    comments added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const socketio = require('socket.io');
 const http = require('http');
 const cors = require('cors');
 
+//simple funcs to manage users and rooms
 const { addUser, removeUser, getUser, getUsersInRoom} = require('./users.js');
 
 const PORT = process.env.PORT || 5000;
@@ -19,6 +20,7 @@ app.use(cors());
 
 
 io.on('connection', socket => {
+    //waiting for user, then create a new one
     socket.on('join', ({ name, room }, callback) => {
         const { error, user } = addUser({ id: socket.id, name, room});
 
@@ -26,9 +28,9 @@ io.on('connection', socket => {
         
         const date = new Date();
         const time = date.getHours() + ':' + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
-
+        //welcome msg for user
         socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}`, time: time });
-
+        //info msg for other users of the room
         socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!`, time: time});
 
         socket.join(user.room);
@@ -37,7 +39,7 @@ io.on('connection', socket => {
 
         callback();
     });
-
+    //waiting for a new message and send it to other users
     socket.on('sendMessage', (message, callback) => {
         const user = getUser(socket.id);
         const date = new Date();
@@ -45,7 +47,7 @@ io.on('connection', socket => {
         io.to(user.room).emit('message', {user: user.name, text: message, time});
         callback();
     });
-
+    //waiting for disconnection to update users list
     socket.on('disconnect', () => {
         const user = removeUser(socket.id);
 
